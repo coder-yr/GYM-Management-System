9cd9429e10b40986c0ca9c5581ddb3248d653297
Yash <yashramt@gmail.com>
2025-06-12 09:04:52 +0530

    some minor changes

--- routes/receiptRoutes.js
+++ routes/receiptRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Receipt = require('../models/Receipt');
+const Receipt = require('../models/receipt');
 const path = require('path');
 const fs = require('fs');
 
